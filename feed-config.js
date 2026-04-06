1d82a9145710fe265109620656442c1bc7394b67
Ian Stinson <signup06@gmail.com>
2026-04-06 09:46:30 -0400

    Disable stale remote feed override

--- feed-config.js
+++ feed-config.js
@@ -1,3 +1,3 @@
 window.resultsFeedConfig = window.resultsFeedConfig || {
-  remoteScriptUrl: "https://script.google.com/macros/s/AKfycbzqEMoJJ4f9EWbWg9KxB9y3ir-MCILlg3jKPsBEjUK1YNQSgqhYPR9fO7xzjAanbcvUEQ/exec?format=js"
+  remoteScriptUrl: ""
 };
